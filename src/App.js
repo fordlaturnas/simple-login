2073879bc9f5081aa3445e8c664686f7450efbda
Ford Laturnas <fordlaturnas1209@gmail.com>
2022-04-02 17:53:07 +0800

    simple-login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, Fragment } from 'react';
 import './App.css';
+import MainHeader from './components/MainHeader/MainHeader';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // by default isLoggedIn is set to false.
+
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {/** If isLoggedIn is not-false return <Login /> */}
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {/** If isLoggedIn is false return <Logout /> */}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </Fragment>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+
+import Button from '../UI/Button/Button';
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+
+// received props form App.js { onLogin }
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+
+  const [emailIsValid, setEmailIsValid] = useState(); // by default undefine - it gives a falsy value.
+  const [passwordIsValid, setPasswordIsValid] = useState(); // by default empty string - it gives a falsy value.
+
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+
+    // base on the condition, it will return true or false.   it will return true if both of the conditions are met.
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+
+    // base on the condition, it will return true or false.   it will return true if both of the conditions are met.
+    setFormIsValid(
+      event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    );
+  };
+
+  const validateEmailHandler = () => {
+    // return true if the conditions are met.
+    setEmailIsValid(enteredEmail.includes('@'));
+  };
+
+  const validatePasswordHandler = () => {
+    // return true if the conditions are met.
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    //"lifting up the state"
+    // pass the state to App.js
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ''
+          }`}
+        >
+          <label>Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
